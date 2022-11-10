26a5023874a5d5d3329dc593a2f01e3c499655a9
salunaalavi <salunaalavi@gmail.com>
2022-11-10 10:43:42 +0700

    Initializing vite and used packages

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: () => ({
+
+    }),
+    mutations: {
+
+    },
+    actions: {
+
+    },
+});
+
+export default store;
